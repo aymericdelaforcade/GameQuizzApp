67f230e589ac817a89ee9d516fbfb9d1c6dcf8f6
Aymeric de Laf <aymericdelaforcade@gmail.com>
2024-06-12 17:43:51 +0200

    ajout url à tous, début logique

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
+import 'react-native-gesture-handler';
+import React, { useReducer } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+import { scale, verticalScale } from 'react-native-size-matters';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Menu from './screens/Menu';
+import { NavigationContainer } from '@react-navigation/native';
+import setDefaultProps from 'react-native-simple-default-props'
+import {useEffect, useState} from 'react';
+
+import * as Font from 'expo-font';
+import GameScreenChoice from './screens/GameScreenChoice';
+import NormalPlay from './screens/NormalPlay';
+
+
+const Stack = createStackNavigator();
+
+
+
 
 export default function App() {
+
+
+  const [fontsLoaded, setFontsLoaded] = React.useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        'PoppinsBlack': require('./assets/fonts/Montserrat-Medium.ttf'),
+      })
+      .then(() => {
+        setDefaultProps(Text, { style: [{ fontFamily: 'PoppinsBlack', color: '#FFFFFF'}], });
+       }) 
+  
+      // Mettre en pause l'exécution pendant 2 secondes
+      //await new Promise(resolve => setTimeout(resolve, 100));
+  
+      //setFontsLoaded(true);
+      //await SplashScreen.hideAsync();
+    }
+
+    loadFonts();
+
+  }, []);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false, presentation: 'transparentModal'}}>
+        <Stack.Screen name="Menu" component={Menu} />
+        <Stack.Screen name="GameScreenChoice" component={GameScreenChoice} />
+        <Stack.Screen name="Normal Play" component={NormalPlay} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/NormalPlay.js
+++ screens/NormalPlay.js
@@ -6,45 +6,98 @@ import * as Progress from 'react-native-progress';
 import { scale, verticalScale } from 'react-native-size-matters';
 
 import Entypo from 'react-native-vector-icons/Entypo'
+import AntDesign from 'react-native-vector-icons/AntDesign'
+
+
 import * as XLSX from 'xlsx';
 
 
 
 export default function NormalPlay({navigation}) {
 
-    const [data, setData] = useState([]);
-
-    const BaseDonnée = require('../assets/BaseDonnée1.json')
-
-    console.log(BaseDonnée.Destiny2['Nom simple'])
-    
+  const [jeuActuel, setjeuActuel] = useState(0);
+  const [Cross1Opacity, setCross1Opacity] = useState(0)
+  const [Cross2Opacity, setCross2Opacity] = useState(0)
+  const [Cross3Opacity, setCross3Opacity] = useState(0)
 
   const [GameInput, setGameInput] = useState('')
 
+  const BaseDonnée = require('../assets/BaseDonnée1.json')
+
+  const Basedonnéeconvertie = Object.entries(BaseDonnée);
+
+  const TailledeBasedonnée = Basedonnéeconvertie.length
+
+  function generateUniqueRandomListeNombreAleatorire(TailledeBasedonnée) { //code chat GPT qui permet de générer l'array de l'ordre des questions
+    const ListeNombreAleatorire = Array.from({ length: TailledeBasedonnée + 1 }, (v, k) => k);
+    for (let i = ListeNombreAleatorire.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [ListeNombreAleatorire[i], ListeNombreAleatorire[j]] = [ListeNombreAleatorire[j], ListeNombreAleatorire[i]];
+    }
+    return ListeNombreAleatorire
+  }
+  generateUniqueRandomListeNombreAleatorire(TailledeBasedonnée)
+
+  console.log(Basedonnéeconvertie[jeuActuel][1]['Nom simple'] + 'nom simple ')
+
+
+  function BonJeuouPas () {
+    if (GameInput == Basedonnéeconvertie[jeuActuel][1]['Nom simple']){
+      return null
+    }else{
+      if(Cross1Opacity == 0){
+        setCross1Opacity(1)
+      }else{
+        if (Cross2Opacity == 0){
+          setCross2Opacity(1)
+        }else{
+          if (Cross3Opacity == 0){
+            setCross3Opacity(1)
+          }else{
+            console.log('trois erreur')
+          }
+        }
+      }
+
+
+    }
+  }
+
+  
+
+  
+
   return (
     <View style={styles.container}>
-        <View style={styles.header}></View>
+        <View style={styles.header}>
+          <Text style={{marginBottom: '2%'}}>0:29</Text>
+          <Progress.Bar progress={0.3} width={scale(100)} height={verticalScale(8)} style={{marginBottom: '15%', borderColor: 'grey'}} color='red'/>
+        </View>
         <View style={styles.viewButtonMiddle}>
-            <Progress.Bar progress={0.3} width={200} />
             <Image
-                source={{ uri: BaseDonnée.Destiny2['URL'] }}
+                source={{ uri: Basedonnéeconvertie[jeuActuel][1]['Nom simple'] }}
                 style={{ width: scale(320), height: verticalScale(170), marginBottom: '5%'}} // Taille de l'image
             />
-            <View style={styles.textInputGame}>
-                <TextInput 
-                    marginTop ={'-1.2%'}
-                    maxLength= {25}
-                    placeholder= 'Game name ?'
-                    value={GameInput}
-                    onChangeText={setGameInput}
-                    color={'#FFFFFF'}
-                    placeholderTextColor={'#FFFFFF'}
-                />
+            <View style={{flexDirection: 'row'}}>
+              <View style={styles.textInputGame}>
+                  <TextInput 
+                      marginTop ={'-1.2%'}
+                      maxLength= {25}
+                      placeholder= 'Game name ?'
+                      value={GameInput}
+                      onChangeText={setGameInput}
+                      color={'#FFFFFF'}
+                      placeholderTextColor={'#FFFFFF'}
+                  />
+              </View>
+              <TouchableOpacity style={styles.textInputGameArrowRight} onPress={() => BonJeuouPas()}>
+                <AntDesign name="arrowright" color={'white'} size={30} />
+              </TouchableOpacity>
             </View>
             <View style={styles.viewdes3essais}>
-                <View style={styles.unboxEssai}><Entypo name="cross" color={'red'} size={25} /></View>
-                <View style={styles.unboxEssai}><Entypo name="cross" color={'red'} size={25} /></View>
-                <View style={styles.unboxEssai}><Entypo name="cross" color={'red'} size={25} /></View>
+                <View style={styles.unboxEssai}><Entypo name="cross" color={'red'} size={25} style={{opacity: Cross1Opacity}}/></View>
+                <View style={styles.unboxEssai}><Entypo name="cross" color={'red'} size={25} style={{opacity: Cross2Opacity}}/></View>
+                <View style={styles.unboxEssai}><Entypo name="cross" color={'red'} size={25} style={{opacity: Cross3Opacity}}/></View>
             </View>
         </View>
 
@@ -70,8 +123,8 @@ const styles = StyleSheet.create({
 
   header:{
     flex: 1.2,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start'
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 
   textInputGame:{
@@ -84,6 +137,19 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: '5%',
     borderWidth: 0.5,
+  },
+
+  textInputGameArrowRight:{
+    height: verticalScale(43),
+    width: scale(50),
+    backgroundColor:'#353535',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: '1%',
+    borderRadius: 10,
+    marginTop: '5%',
+    borderWidth: 0.5,
+    marginLeft: '1%'
   },
 
   viewdes3essais:{
